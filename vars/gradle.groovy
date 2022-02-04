ca3fb1001e2eeabc9dd3787e0173719cdda7e3c4
mufasa2019 <cigodoyc@gmail.com>
2022-02-04 20:50:15 -0300

    clone var 2

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -1,106 +1,80 @@
-import utilities.*
-
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
 def call(stages){
-
-    //def stagesList = stages.split(";")
-    // stagesList.each{
-    //     println("===>${it}")
-    //     "${it}"()
-
-    // }
-
-   def listStagesOrder = [
+​
+    def listStagesOrder = [
         'build': 'stageCleanBuildTest',
         'sonar': 'stageSonar',
-        'curl_spring': 'stageRunSpringCurl',
+        'run_spring_curl': 'stageRunSpringCurl',
         'upload_nexus': 'stageUploadNexus',
         'download_nexus': 'stageDownloadNexus',
         'run_jar': 'stageRunJar',
         'curl_jar': 'stageCurlJar'
     ]
-
-    def arrayUtils = new array.arrayExtentions();
-    def stagesArray = []
-        stagesArray = arrayUtils.searchKeyInArray(stages, ";", listStagesOrder)
-
-    if (stagesArray.isEmpty()) {
+​
+    if (stages.isEmpty()) {
         echo 'El pipeline se ejecutará completo'
         allStages()
     } else {
         echo 'Stages a ejecutar :' + stages
-        stagesArray.each{ stageFunction ->//variable as param
-            echo 'Ejecutando ' + stageFunction
-            "${stageFunction}"()
+        listStagesOrder.each { stageName, stageFunction ->
+            stages.each{ stageToExecute ->//variable as param
+                if(stageName.equals(stageToExecute)){
+                echo 'Ejecutando ' + stageFunction
+                "${stageFunction}"()
+                }
+            }
         }
+​
     }
 ​
-//     if (stages.isEmpty()) {
-//         echo 'El pipeline se ejecutará completo'
-//         allStages()
-//     } else {
-//         echo 'Stages a ejecutar :' + stages
-//         listStagesOrder.each { stageName, stageFunction ->
-//             stagesList.each{ stageToExecute ->//variable as param
-//                 if(stageName.equals(stageToExecute)){
-//                 echo 'Ejecutando ' + stageFunction
-//                 "${stageFunction}"()
-//                 }
-//             }
-//         }
-// ​
-//     }
 }
-
-def allStages(){
-    stageCleanBuildTest()
-    stageSonar()
-    stageRunSpringCurl()
-    stageUploadNexus()
-    stageDownloadNexus()
-    stageRunJar()
-    stageCurlJar()
-}
-
+​
 def stageCleanBuildTest(){
-    env.TAREA = "Paso 1: Build && Test"
-    stage("$env.TAREA"){
-        sh "echo 'Build && Test!'"
+    env.DESCRTIPTION_STAGE = 'Paso 1: Build - Test'
+    stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "build - ${env.DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
         sh "gradle clean build"
-        // code
     }
 }
-
+​
 def stageSonar(){
-    env.TAREA="Paso 2: Sonar - Análisis Estático"
-    stage("$env.TAREA"){
-        sh "echo 'Análisis Estático!'"
+    env.DESCRTIPTION_STAGE = "Paso 2: Sonar - Análisis Estático"
+    stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "sonar - ${DESCRTIPTION_STAGE}"
         withSonarQubeEnv('sonarqube3') {
-            sh "echo 'Calling sonar by ID!'"
-            // Run Maven on a Unix agent to execute Sonar.
-            sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
+            sh "echo  ${env.STAGE}"
+            sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
         }
     }
 }
-
+​
 def stageRunSpringCurl(){
-    env.TAREA="Paso 3: Curl Springboot Gradle sleep 20"
-    stage("$env.TAREA"){
+    env.DESCRTIPTION_STAGE = "Paso 3: Curl Springboot Gralde sleep 20"
+    stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "run_spring_curl - ${DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
         sh "gradle bootRun&"
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
 }
-
+​
+​
 def stageUploadNexus(){
-    env.TAREA="Paso 4: Subir Nexus"
-    stage("$env.TAREA"){
+    env.DESCRTIPTION_STAGE = "Paso 4: Subir Nexus"
+    stage("${env.DESCRTIPTION_STAGE}"){
         nexusPublisher nexusInstanceId: 'nexus3',
         nexusRepositoryId: 'devops-usach-nexus',
         packages: [
             [$class: 'MavenPackage',
                 mavenAssetList: [
                     [classifier: '',
-                    extension: 'jar',
-                    filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar'
+                    extension: '.jar',
+                    filePath: 'build/DevOpsUsach2020-0.0.1.jar'
                 ]
             ],
                 mavenCoordinate: [
@@ -111,28 +85,45 @@ def stageUploadNexus(){
                 ]
             ]
         ]
+        env.STAGE = "upload_nexus - ${DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
     }
 }
+​
 def stageDownloadNexus(){
-    // env.TAREA="Paso 3: Curl Springboot Gradle sleep 20"
-    // stage("$env.TAREA"){
-    stage("Paso 5: Descargar Nexus"){
-
-        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
+    env.DESCRTIPTION_STAGE = "Paso 5: Descargar Nexus"
+   stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "download_nexus - ${DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        sh ' curl -X GET -u $NEXUS_USER:$NEXUS_PASSWORD "http://nexus3:8081/repository/devops-usach-nexus/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar" -O'
     }
 }
+​
 def stageRunJar(){
-      // env.TAREA="Paso 3: Curl Springboot Gradle sleep 20"
-    // stage("$env.TAREA"){
-    stage("Paso 6: Levantar Artefacto Jar"){
-        sh 'nohup java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
+    env.DESCRTIPTION_STAGE = "Paso 6: Levantar Artefacto Jar"
+    stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "run_jar - ${DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
+        sh 'nohup bash java -jar DevOpsUsach2020-0.0.1.jar & >/dev/null'
     }
 }
+​
 def stageCurlJar(){
-      // env.TAREA="Paso 3: Curl Springboot Gradle sleep 20"
-    // stage("$env.TAREA"){
-    stage("Paso 7: Testear Artefacto - Dormir(Esperar 20sg) "){
+    env.DESCRTIPTION_STAGE = "Paso 7: Testear Artefacto - Dormir Esperar 20sg "
+    stage("${env.DESCRTIPTION_STAGE}"){
+        env.STAGE = "curl_jar - ${DESCRTIPTION_STAGE}"
+        sh "echo  ${env.STAGE}"
         sh "sleep 20 && curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
+}
+​
+def allStages(){
+    stageCleanBuildTest()
+    stageSonar()
+    stageRunSpringCurl()
+    stageUploadNexus()
+    stageDownloadNexus()
+    stageRunJar()
+    stageCurlJar()
 }
 return this;
