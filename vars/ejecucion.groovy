e944d5f918fe4fd085f32a47bf1eb0d1ba101fef
mufasa2019 <cigodoyc@gmail.com>
2022-02-04 21:02:34 -0300

    clone var 4

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -1,37 +1,53 @@
+/*
+
+	forma de invocación de método call:
+
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+
+*/
+
 def call(){
-  pipeline {
-      agent any
-      environment {
-          NEXUS_USER         = credentials('NEXUS-USER')
-          NEXUS_PASSWORD     = credentials('NEXUS-PASS')
-      }
-      parameters {
-            choice choices: ['maven', 'gradle'], description: 'Seleccione una herramienta para preceder a compilar', name: 'compileTool'
-            text description: 'Enviar los stages separados por ";"... Vacío si necesita todos los stages', name: 'stages'
-      }
-      stages {
-          stage("Pipeline"){
-              steps {
-                  script{
-                      sh "env"
-                      env.TAREA = ""
-                      if(params.compileTool == 'maven'){
-                        maven.call(params.stages);
-                      }else{
-                        gradle.call(params.stages)
-                      }
-                  }
-              }
-              post{
-          success{
-            //slackSend color: 'good', message: "[Mentor] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431'
-          }
-          failure{
-            //slackSend color: 'danger', message: "[Mentor] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431'
-          }
+    pipeline {
+        agent any
+        environment {
+            NEXUS_USER         = credentials('NEXUS-USER')
+            NEXUS_PASSWORD     = credentials('NEXUS-PASS')
+            }
+        parameters {
+            choice(
+                name:'compileTool',
+                choices: ['Maven', 'Gradle'],
+                description: 'Seleccione herramienta de compilacion'
+            )
+            text description: '''enviar los stages separados por ";" Vacio si necesita todos los stages''', name: 'stages'
         }
-          }
-      }
-  }
+        stages {
+            stage("Pipeline"){
+                steps {
+                    script{
+                    switch(params.stages)
+                        {
+                            case 'Maven':
+                                maven.call(params.stages);
+                            break;
+                            case 'Gradle':
+                                gradle.call(params.stages);
+                            break;
+                        }
+                    }
+                }
+                post{
+                    success{
+                        //slackSend color: 'good', message: "[Cristian] [${JOB_NAME}] [${BUILD_TAG}] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'Slack01'
+                    }
+                    failure{
+                        //slackSend color: 'danger', message: "[Cristian] [${env.JOB_NAME}] [${BUILD_TAG}] Ejecucion fallida en stage [${env.TAREA}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'Slack01'
+                    }
+                }
+            }
+        }
+    }
 }
+
 return this;
