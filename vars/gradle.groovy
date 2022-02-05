81f76872ae65044b409efdb1991fdcb78345af0f
mufasa2019 <cigodoyc@gmail.com>
2022-02-04 21:49:55 -0300

    clone var 8

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -40,7 +40,24 @@ def rest(){
 }
 
 def nexus(){
-	nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'devops-usach-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build/libs/DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+nexusPublisher nexusInstanceId: 'nexus',
+    nexusRepositoryId: 'devops-usach-nexus',
+    packages: [
+        [$class: 'MavenPackage',
+            mavenAssetList: [
+                [classifier: '',
+                extension: 'jar',
+                filePath: 'build/DevOpsUsach2020-0.0.1.jar'
+            ]
+        ],
+            mavenCoordinate: [
+                artifactId: 'DevOpsUsach2020',
+                groupId: 'com.devopsusach2020',
+                packaging: 'jar',
+                version: '0.0.1'
+            ]
+        ]
+    ]
 }
 
 return this;
